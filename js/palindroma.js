2b1dff04c974c3cd6dfe5c2e95a1e587c5379222
Giovanni-Damico98 <gd.installazioni25@gmail.com>
2024-08-28 01:26:50 +0200

    JS progress

---
+++ js/palindroma.js
@@ -0,0 +1,29 @@
+/* Chiedere all'utente di inserire una parola 
+-  Creare una funzione per capire se la parola inserita è palindroma */
+console.log("JS OK");
+// Preparazione
+function isPalindroma(word) {
+  // Pulizia dei dati
+  word = word.trim().toLowerCase();
+  // Capovolgo la parola
+  const reversedWord = word.split("").reverse().join("");
+  // Controllo se la parola capovolta è uguale alla parola non capovolta
+  return reversedWord === word;
+}
+
+// Rerecupero gli elementi
+const form = document.getElementById("palindroma-form");
+const wordField = document.getElementById("word");
+const resultElement = document.getElementById("result");
+
+form.addEventListener("submit", function (event) {
+  // Blocco il riavvio della pagina
+  event.preventDefault();
+  // Raccolta dati
+  const word = wordField.value.trim();
+  // Output
+  resultElement.innerText = isPalindroma(word)
+    ? `La parola ${word} è palindroma`
+    : `La parola ${word} non è palindroma`;
+  // Validazione
+});
