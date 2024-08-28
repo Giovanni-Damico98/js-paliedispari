11d7dafc704a24cbaa6af677cdad6ea5f96730a1
Giovanni-Damico98 <gd.installazioni25@gmail.com>
2024-08-28 02:03:20 +0200

    pariodispari progress html + JS

--- js/functions.js
+++ js/functions.js
@@ -16,3 +16,26 @@ function isPalindroma(word) {
   // Controllo se la parola capovolta è uguale alla parola non capovolta
   return reversedWord === word;
 }
+
+/**
+ * Genera un numero intero casuale tra due valori specificati.
+ *
+ * @param {number} [min=1] - Il valore minimo del range (incluso).
+ * @param {number} [max=100] - Il valore massimo del range (incluso o escluso a seconda di `isMaxIncluded`).
+ * @param {boolean} [isMaxIncluded=true] - Indica se il valore massimo (`max`) è incluso nel range.
+ * @returns {number|null} - Un numero intero casuale tra `min` e `max` (incluso o escluso), `NaN` se i parametri non sono numeri, o `null` se `min` è maggiore o uguale a `max`.
+ */
+function getRandomNumber(min = 1, max = 100, isMaxIncluded = true) {
+  if (isNaN(min) || isNaN(max)) {
+    console.error("Min e Max devono essere numeri");
+    return NaN;
+  }
+
+  if (min >= max) {
+    console.error(`Errore nei parametri: ${min} è superiore o uguale a ${max}`);
+    return null;
+  }
+
+  if (isMaxIncluded) max++;
+  return Math.floor(Math.random() * (max - min)) + min;
+}
---
+++ js/pariodispari.js
@@ -0,0 +1,42 @@
+// Funzioni
+function getRandomNumber(min = 1, max = 100, isMaxIncluded = true) {
+  if (isNaN(min) || isNaN(max)) {
+    console.error("Min e Max devono essere numeri");
+    return NaN;
+  }
+
+  if (min >= max) {
+    console.error(`Errore nei parametri ${min} è superiore o uguale a ${max}`);
+    return null;
+  }
+
+  if (isMaxIncluded) max++;
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+// Preparazione
+const form = document.getElementById("pariodispari-form");
+const numberField = document.getElementById("number");
+const choiceField = document.getElementById("choice");
+const resultElement = document.getElementById("result");
+
+//Intercetto l'invio del form
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  // Genero un numero casuale per il pc
+  const cpuNumb = getRandomNumber(1, 5);
+  // Recupero dati
+  const userNumb = parseInt(numberField.value);
+  const userChoice = choiceField.value;
+  // Validazione
+  if (isNaN(userNumb || userNumb > 5 || userNumb < 1)) {
+    alert("Il numero deve essere compreso tra 1 e 5");
+    return;
+  }
+
+  if ((userChoice !== "odd") & (userChoice !== "even")) {
+    alert("Devi scegliere pari o dispari");
+    return;
+  }
+
+  // Elaborazione
+});
